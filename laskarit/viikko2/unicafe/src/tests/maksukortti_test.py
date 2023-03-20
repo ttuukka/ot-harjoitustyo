a95fdb7983980712413e09f0642b47ae62768575
Tuukka Tamminen <tuukka.tamminen@helsinki.fi>
2023-03-20 15:52:35 +0200

    aloitetaan alusta

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from maksukortti import Maksukortti
-
-class TestMaksukortti(unittest.TestCase):
-    def setUp(self):
-        self.maksukortti = Maksukortti(1000)
-
-    def test_luotu_kortti_on_olemassa(self):
-        self.assertNotEqual(self.maksukortti, None)
